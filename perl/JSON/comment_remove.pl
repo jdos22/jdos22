b5d1f4db0473dc60fa74f0c14f4940069808a757
Jeff Sumner <kc4fox@gmail.com>
2023-04-08 09:05:12 -0400

    Found regexp::common documentation

--- perl/JSON/comment_remove.pl
+++ perl/JSON/comment_remove.pl
@@ -1,9 +1,18 @@
 use Modern::Perl;
-use Regexp::Common::comment;
+use Regexp::Common;
 
 # Purpose:
 # Get rid of // style comments from kea configuration.
 # because the JSON handler doesn't do it.
+# Going to try to use regexp::Common::Comment.
+# The following regexes are what I think are appropriate:
+# /$RE{comment}{C}/
+# /$RE{comment}{C++}/
+# /$RE{commont}{shell}/
+# So remember that the comments that are hard are not the single line comments
+# but the ones ot the end of the line. Or maybe worse - the ones between 
+# delimiters
+
 
 while (<DATA>) {
 	chomp;
