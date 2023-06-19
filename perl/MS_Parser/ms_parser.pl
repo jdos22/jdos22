7caa3a43712cf6acae49dd3499a2390201ed0197
Jeff Sumner <87584599+jdos22@users.noreply.github.com>
2023-06-19 07:02:50 -0400

    Added Extra Data

--- perl/MS_Parser/ms_parser.pl
+++ perl/MS_Parser/ms_parser.pl
@@ -10,8 +10,22 @@ use Data::Dumper;
 
 my @Extra_List = [
 	"c1.garbanet. A	192.168.25.156\n".
-	"c2.garbanet. A	192.168.26.200\n"
+	"c2.garbanet. A	192.168.26.200\n".
+	"Garbage.net. A NotIP192.168.26.201\n"
 ];
 my @MS_Test = ["hostname","192.168.0.200",@Extra_List];
 
-say Dumper @MS_Test;
+say $MS_Test[0]->[2]->[0];
+my @Big_List = split '\n',$MS_Test[0]->[2]->[0];
+
+foreach my $Line (@Big_List) {
+	say "Begnning";
+	say $Line;
+	say "End";
+	my @Elements = split ' ',$Line;
+	say join ' --- ',@Elements;
+	my $RE = qr/^(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))$/;
+	if (($Elements[1] eq 'A') and ($Elements[2]=~ $RE )) {
+		say "Found A record with a passable IP.";
+	}
+}
