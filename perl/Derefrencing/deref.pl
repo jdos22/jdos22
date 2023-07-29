3aeff83e0de344ad130c701fb3aac0f2d1e4da3e
Jeff Sumner <87584599+jdos22@users.noreply.github.com>
2023-07-28 21:04:23 -0400

    Created to play with references

--- perl/Derefrencing/deref.pl
+++ perl/Derefrencing/deref.pl
@@ -1,2 +1,13 @@
 #!/usr/bin/env perl
 
+# Dereferencing has become very important as I work with different data
+# structures. I've found that there are some subtle differences in the way
+# that the dereferencing is handled and I'd like to get a better handle on 
+# exactly how I *should* be doing things.
+use Modern::Perl;
+use Data::Dumper;
+
+my %Array;
+$Array{"first"} = [ "First Element", "Second Element"];
+
+say Dumper %Array;
